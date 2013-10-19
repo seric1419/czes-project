eb95876fef5af96846022e188330ead44b9e65cc
sk42884@st.amu.edu.pl <sk42884@st.amu.edu.pl@f2dd733a-1106-00e9-de2c-b5f7bbc8cc4e>
2013-10-19 18:41:40 +0000

    1. Obrazki - stałe finalne

--- trunk/czes-project/Chess-game-klient/src/interfejs/SzachownicaGUI.java
+++ trunk/czes-project/Chess-game-klient/src/interfejs/SzachownicaGUI.java
@@ -4,16 +4,15 @@ import interfejs.ImagePanel.ZmianaWybranegoListener;
 
 import java.awt.Color;
 import java.awt.EventQueue;
-import java.awt.image.BufferedImage;
-import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import commons.Constants;
 
 public class SzachownicaGUI extends JFrame implements ZmianaWybranegoListener{
 
@@ -94,52 +93,39 @@ public class SzachownicaGUI extends JFrame implements ZmianaWybranegoListener{
 	
 	private void inicjalizujFigury(){
 		try{
-			BufferedImage czarnyPion = ImageIO.read(new File("images\\czarny_pion.png"));
-			BufferedImage bialyPion = ImageIO.read(new File("images\\bialy_pion.png"));
-			BufferedImage czarnaWieza = ImageIO.read(new File("images\\czarna_wieza.png"));
-			BufferedImage bialaWieza = ImageIO.read(new File("images\\biala_wieza.png"));
-			BufferedImage czarnyKon = ImageIO.read(new File("images\\czarny_kon.png"));
-			BufferedImage bialyKon = ImageIO.read(new File("images\\bialy_kon.png"));
-			BufferedImage czarnyGoniec = ImageIO.read(new File("images\\czarny_goniec.png"));
-			BufferedImage bialyGoniec = ImageIO.read(new File("images\\bialy_goniec.png"));
-			BufferedImage czarnaKrolowa = ImageIO.read(new File("images\\czarna_krolowa.png"));
-			BufferedImage bialaKrolowa = ImageIO.read(new File("images\\biala_krolowa.png"));
-			BufferedImage czarnyKrol = ImageIO.read(new File("images\\czarny_krol.png"));
-			BufferedImage bialyKrol = ImageIO.read(new File("images\\bialy_krol.png"));
+			getPole(0,0).setImage(Constants.czarnaWieza);
+			getPole(1,0).setImage(Constants.czarnyKon);
+			getPole(2,0).setImage(Constants.czarnyGoniec);
+			getPole(3,0).setImage(Constants.czarnaKrolowa);
+			getPole(4,0).setImage(Constants.czarnyKrol);
+			getPole(5,0).setImage(Constants.czarnyGoniec);
+			getPole(6,0).setImage(Constants.czarnyKon);
+			getPole(7,0).setImage(Constants.czarnaWieza);
+			getPole(0,1).setImage(Constants.czarnyPion);
+			getPole(1,1).setImage(Constants.czarnyPion);
+			getPole(2,1).setImage(Constants.czarnyPion);
+			getPole(3,1).setImage(Constants.czarnyPion);
+			getPole(4,1).setImage(Constants.czarnyPion);
+			getPole(5,1).setImage(Constants.czarnyPion);
+			getPole(6,1).setImage(Constants.czarnyPion);
+			getPole(7,1).setImage(Constants.czarnyPion);
 			
-			getPole(0,0).setImage(czarnaWieza);
-			getPole(1,0).setImage(czarnyKon);
-			getPole(2,0).setImage(czarnyGoniec);
-			getPole(3,0).setImage(czarnaKrolowa);
-			getPole(4,0).setImage(czarnyKrol);
-			getPole(5,0).setImage(czarnyGoniec);
-			getPole(6,0).setImage(czarnyKon);
-			getPole(7,0).setImage(czarnaWieza);
-			getPole(0,1).setImage(czarnyPion);
-			getPole(1,1).setImage(czarnyPion);
-			getPole(2,1).setImage(czarnyPion);
-			getPole(3,1).setImage(czarnyPion);
-			getPole(4,1).setImage(czarnyPion);
-			getPole(5,1).setImage(czarnyPion);
-			getPole(6,1).setImage(czarnyPion);
-			getPole(7,1).setImage(czarnyPion);
-			
-			getPole(0,6).setImage(bialyPion);
-			getPole(1,6).setImage(bialyPion);
-			getPole(2,6).setImage(bialyPion);
-			getPole(3,6).setImage(bialyPion);
-			getPole(4,6).setImage(bialyPion);
-			getPole(5,6).setImage(bialyPion);
-			getPole(6,6).setImage(bialyPion);
-			getPole(7,6).setImage(bialyPion);
-			getPole(0,7).setImage(bialaWieza);
-			getPole(1,7).setImage(bialyKon);
-			getPole(2,7).setImage(bialyGoniec);
-			getPole(3,7).setImage(bialaKrolowa);
-			getPole(4,7).setImage(bialyKrol);
-			getPole(5,7).setImage(bialyGoniec);
-			getPole(6,7).setImage(bialyKon);
-			getPole(7,7).setImage(bialaWieza);
+			getPole(0,6).setImage(Constants.bialyPion);
+			getPole(1,6).setImage(Constants.bialyPion);
+			getPole(2,6).setImage(Constants.bialyPion);
+			getPole(3,6).setImage(Constants.bialyPion);
+			getPole(4,6).setImage(Constants.bialyPion);
+			getPole(5,6).setImage(Constants.bialyPion);
+			getPole(6,6).setImage(Constants.bialyPion);
+			getPole(7,6).setImage(Constants.bialyPion);
+			getPole(0,7).setImage(Constants.bialaWieza);
+			getPole(1,7).setImage(Constants.bialyKon);
+			getPole(2,7).setImage(Constants.bialyGoniec);
+			getPole(3,7).setImage(Constants.bialaKrolowa);
+			getPole(4,7).setImage(Constants.bialyKrol);
+			getPole(5,7).setImage(Constants.bialyGoniec);
+			getPole(6,7).setImage(Constants.bialyKon);
+			getPole(7,7).setImage(Constants.bialaWieza);
 		} catch (Exception e){
 			e.printStackTrace();
 		}
